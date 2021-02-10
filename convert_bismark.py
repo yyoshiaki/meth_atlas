1374d2dd4a0106e44b5444297e94fad731b10a7d
yyoshiaki <y.yasumizu@gmail.com>
2021-02-10 16:15:45 +0900

    mm10

--- convert_bismark.py
+++ convert_bismark.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser(description='convert bismark for meth_atlas')
 parser.add_argument('file', type=str, help='input bismark file')
 parser.add_argument('sample_name', type=str, help='sample name')
 parser.add_argument('--tile', type=int, default=0, help='If tile windows, specify window size (bp).')
+parser.add_argument('--reference', type=str, default='hg38', help='hg38 or mm10')
 parser.add_argument('--th_cov', type=int, default=0, help='Threshold of coverage within CpG or tile.')
 parser.add_argument('--bedtools', type=str, default='bedtools', help='Full path to bedtools')
 
@@ -19,17 +20,21 @@ f = args.file
 n = args.sample_name
 t = args.tile
 # th_cov = args.th_cov
+ref = args.reference
 th_cov = 5
 p_bedtools = args.bedtools
 
+if not ref in ['hg38', 'mm10']:
+    raise ValueError(ref + " is an invalid reference.")
+
 def check_tile(t):
-    if not os.path.exists('{d}/data/hg38.win{t}.bed.gz'.format(d=os.path.dirname(os.path.abspath(__file__)), t=t)):
-        cmd = '{b} makewindows -g {d}/data/hg38.sort.genome -w {t} > {d}/data/hg38.win{t}.bed'.format(
-            b=p_bedtools, d=os.path.dirname(os.path.abspath(__file__)), t=t)
+    if not os.path.exists('{d}/data/{r}.win{t}.bed.gz'.format(r=ref, d=os.path.dirname(os.path.abspath(__file__)), t=t)):
+        cmd = '{b} makewindows -g {d}/data/{r}.sort.genome -w {t} > {d}/data/{r}.win{t}.bed'.format(
+            b=p_bedtools, d=os.path.dirname(os.path.abspath(__file__)), t=t, r=ref)
         print(cmd)
         subprocess.run(cmd, shell=True)
 
-        cmd = 'gzip {d}/data/hg38.win{t}.bed'.format(d=os.path.dirname(os.path.abspath(__file__)), t=t)
+        cmd = 'gzip {d}/data/{r}.win{t}.bed'.format(r=ref, d=os.path.dirname(os.path.abspath(__file__)), t=t)
         print(cmd)
         subprocess.run(cmd, shell=True)
 
@@ -40,9 +45,9 @@ if t != 0:
     print(cmd)
     subprocess.run(cmd, shell=True)
 
-    cmd = '{b} map -a {d}/data/hg38.win{x}.bed.gz -b {bis} -c 4,5,6 -o mean,sum,sum | grep -v "\.\s*\." > {o}'.format(
+    cmd = '{b} map -a {d}/data/{r}.win{x}.bed.gz -b {bis} -c 4,5,6 -o mean,sum,sum | grep -v "\.\s*\." > {o}'.format(
         b = p_bedtools, d=os.path.dirname(os.path.abspath(__file__)),
-        x = t, bis='tmp.sort.txt', o='tmp.tile.txt')
+        x = t, bis='tmp.sort.txt', o='tmp.tile.txt', r=ref)
     print(cmd)
     subprocess.run(cmd, shell=True)
     f = 'tmp.tile.txt'
